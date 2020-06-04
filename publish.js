3d7b45070f753a88991eb91c8bf9dbb6333e540d
jxuxaxcxo <joaquin.vargaselio@gmail.com>
2020-06-03 22:24:10 -0400

    JSON construction

--- publish.js
+++ publish.js
@@ -8,10 +8,28 @@ var client  = MQTT.connect(Broker, {clientId: Client});
 client.on("connect", function(){
     setInterval(function(){ 
         console.log("Message Sent")
-        client.publish(TopicName, "Example");
+        client.publish(TopicName, getJSON());
     }, 5000);
 }); 
 
+function getJSON(){
+  var d = new Date();
+  var year = d.getFullYear();
+  var month = d.getMonth();
+  var day = d.getUTCDay();
+  var date = d.getDate();
+  var hours = d.getHours();
+  var minutes = d.getMinutes();
+  var seconds = d.getSeconds();
+  var miliSeconds = d.getMilliseconds();
 
+  var dates = ["Mon","Tue","Wed","Thu","Wed","Fri","Sat","Sun"]
+  var months = ["Jan","Feb","Apr","Mar","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 
+  var ip = "127.0.0.1"
 
+  var name = "Dugstein"
+  var json = `{ \"time\" : \"${dates[day]} ${months[month]} ${date} ${hours}:${minutes}:${seconds}-${miliSeconds} ${year}\", \"name\" : ${name}, \"ip\" : \"${ip}\"}`
+
+  return json;
+}
